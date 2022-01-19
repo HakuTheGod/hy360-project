78d7c3c86e1cf11777228bc1232d7b77b7568144
Nikola Kovatsevits <hitokichi13@gmail.com>
2022-01-19 22:13:19 +0200

    Fixes

--- src/java/WebApp/src/java/database/tables/EditUserAccountTable.java
+++ src/java/WebApp/src/java/database/tables/EditUserAccountTable.java
@@ -1,9 +1,12 @@
 
 package database.tables;
+import com.google.gson.Gson;
 import database.Database_Connection;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mainClasses.Private_account;
 import mainClasses.Company_account;
 
@@ -13,20 +16,59 @@ public class EditUserAccountTable{
 //ADD USER
 
 //SQL ADD idiwth/COMPANY
-private static final String INSERT_PRIVATE_ACCOUNT = "INSERT INTO Private_account"
-            +"(Private_id, Private_name, Credit_line, Debt, Credit_balance) VALUES"
-            +"(?, ?, ?, ?, ?)";
 
 private static final String INSERT_COMPANY_ACCOUNT = "INSERT INTO Company_account"
             +"(Company_id, Company_name, Credit_line, Debt, Credit_balance)"
             +"(?, ?, ?, ?, ?)";
+
+public Private_account jsonToPrivateAccount(String json){
+    
+    Gson gson = new Gson();
+    
+    Private_account pa = gson.fromJson(json, Private_account.class);
+    return pa;
+    
+}
+
+public void addPrivateAccountFromJSON(String json){
+    Private_account pa = jsonToPrivateAccount(json);
+    insertPrivateAccount(pa);
+}
+
+public void insertPrivateAccount(Private_account pa){
+    try {
+        Connection con = Database_Connection.getConnection();
+        Statement stmt = con.createStatement();
+        String insertQuery = "INSERT INTO Private_account"
+            +"(Private_id, Private_name, Credit_line, Debt, Credit_balance) VALUES"
+            +"("
+                + "'" + pa.getUserID() + "',"
+                + "'" + pa.getUserName() + "',"
+                + "'" + pa.getCredit_line() + "',"
+                + "'" + pa.getDebt() + "',"
+                + "'" + pa.getCredit_balance() + "'"
+                + ")";
+        
+        System.out.println(insertQuery);
+        stmt.executeUpdate(insertQuery);
+        System.out.println("# The private account was successfully inserted in the database.");
+        
+        stmt.close();
+        
+    } catch (SQLException ex) {
+        Logger.getLogger(EditUserAccountTable.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (ClassNotFoundException ex) {
+        Logger.getLogger(EditUserAccountTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+}
 
 //NEW idiwths
     public void insert_private_account(Private_account pa) throws SQLException, ClassNotFoundException{
             Connection con = Database_Connection.getConnection();
             ResultSet rs;
             PreparedStatement prepared_stat;
-            prepared_stat = con.prepareStatement(INSERT_PRIVATE_ACCOUNT);
+            prepared_stat = con.prepareStatement("");
             prepared_stat.setInt(1,pa.getUserID());
             prepared_stat.setString(2,pa.getUserName());
             prepared_stat.setDouble(3,pa.getCredit_line());
@@ -34,6 +76,8 @@ private static final String INSERT_COMPANY_ACCOUNT = "INSERT INTO Company_accoun
             prepared_stat.setDouble(5,pa.getCredit_balance());
             System.out.println(prepared_stat);
             rs = prepared_stat.executeQuery();
+            
+            
             //Credit_balance == credit_line sthn arxh afou den exei 3ode4ei tpt akoma
     }
 
